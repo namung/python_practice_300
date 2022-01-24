e16d943d068c39e7c0dee48eef70882c6dfb093a
namung <92897865+namung@users.noreply.github.com>
2022-01-24 22:08:24 +0900

    Create 011~020.py

---
+++ 011~020.py
@@ -0,0 +1,50 @@
+# 11
+삼성전자=50000
+print(삼성전자 * 10)
+
+# 12
+시가총액 = 2980000000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+# 13
+s = "hello"
+t = "python"
+print(s+"!",t)
+
+# 14
+print(2 + 2 * 3)
+
+# 15
+a = "132"
+print(type(a))
+
+# 16
+num_str="720"
+num_str=int(num_str)
+print(type(num_str))
+
+# 17
+num = 100
+num = str(num)
+print(type(num))
+
+# 18
+num = "15.79"
+num = float(num)
+print(type(num), num)
+
+# 19
+year = "2020"
+year = int(year)
+year1 = year + 1
+year2 = year + 2
+print(year, year1, year2)
+
+# 20
+airc = 48584
+total = airc * 36
+print("총 금액 : " + str(total) + " 원")
