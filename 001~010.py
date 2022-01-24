d76852f625f77a5803ff2b74df0aa13c2901ad3c
namung <92897865+namung@users.noreply.github.com>
2022-01-24 22:04:26 +0900

    Create 001~010.py

---
+++ 001~010.py
@@ -0,0 +1,29 @@
+# 1
+print("Hello World")
+
+# 2
+print("Mary's cosmetics")
+
+# 3
+print("신씨가 소리질렀다. \"도둑이야\".")
+
+# 4
+print("\"C:\\Windows\"")
+
+# 5
+print("안녕하세요. \n만나서 \t\t반갑습니다.")# \n : 줄바꿈(개행), \t : tab  
+
+# 6
+print("오늘은", "일요일") # "," : 띄어쓰기 
+
+# 7
+print("naver;kakao;samsung") # 답 : print("naver","kakao","samsung", sep=";")
+
+# 8
+print("naver","kakao","sk","samsung",sep="/")
+
+# 9
+print("first", end=""); print("second")
+
+# 10
+print(5/3)
